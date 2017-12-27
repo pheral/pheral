59662d16c47d65d5ea59972af75228df25f7e7fe
Dmitriy Yudin <d.yudin@vkostume.ru>
2017-12-28 00:21:21 +0300

    edit index

--- preload/loader.php
+++ preload/loader.php
@@ -1,7 +1,3 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-
-$app = new Pheral\Essential\Application(__DIR__ . '/..');
-
-$app->force('templates/home/index.php');
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../preload/loader.php';
+
+$app = new Pheral\Essential\Application(__DIR__ . '/../');
+
+$app->force('templates/home/index.php');

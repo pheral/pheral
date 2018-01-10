c1a693ce4c434f0c3e25e346ad85be253579cef4
Dmitriy Yudin <d.yudin@vkostume.ru>
2018-01-10 18:39:04 +0300

    load controller by application

--- public/index.php
+++ public/index.php
@@ -4,8 +4,6 @@ require_once __DIR__ . '/../preload/loader.php';
 
 $app = new Pheral\Essential\Application(__DIR__ . '/../');
 
-$app->handle();
-
-$controller = new \App\Network\Controllers\HomeController();
-
-$controller->index();
+$app->handle(
+    new \Pheral\Essential\Network\Request()
+);
